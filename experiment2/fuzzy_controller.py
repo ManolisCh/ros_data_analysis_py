1583b66ca52f1cd80f26e66c88b7ee480ab7bf63
ManolisCh <manolis.chiou@gmail.com>
2016-04-15 13:28:43 +0100

    first fuzzy controller

--- experiment2/fuzzy_controller.py
+++ experiment2/fuzzy_controller.py
@@ -1,28 +1,29 @@
+from docutils.nodes import rubric
+
 import skfuzzy as fuzz
 import numpy as np
 
 # fuzzy sets
 crisp_error = fuzz.Antecedent(np.arange(0, 0.11, 0.01), 'error')
-fuzzy_change = fuzz.Consequent(np.arange(0, 1.1, 0.1), 'change_control')
+fuzzy_change = fuzz.Consequent(np.arange(-1, 1.1, 0.1), 'change_control')
 
 # fuzzy memberships
 crisp_error['small'] = fuzz.trapmf(crisp_error.universe, [0, 0, 0.04, 0.06])
 crisp_error['medium'] = fuzz.trapmf(crisp_error.universe, [0.04, 0.07, 0.08, 0.09])
 crisp_error['large'] = fuzz.trapmf(crisp_error.universe, [0.08, 0.09, 0.1, 0.1])
-fuzzy_change['no_change'] = fuzz.sigmf(fuzzy_change.universe, 0.8, -50)
-fuzzy_change['change'] = fuzz.sigmf(fuzzy_change.universe, 0.8, 40)
+fuzzy_change['no_change'] = fuzz.trimf(fuzzy_change.universe, [-1, -1, 0])
+fuzzy_change['change'] = fuzz.trimf(fuzzy_change.universe, [0, 1, 1])
 
 # fuzzy rules
 rule1 = fuzz.Rule(crisp_error['small'], fuzzy_change['no_change'])
-rule2 = fuzz.Rule(crisp_error['medium'], fuzzy_change['no_changechange'])
+rule2 = fuzz.Rule(crisp_error['medium'], fuzzy_change['no_change'])
 rule3 = fuzz.Rule(crisp_error['large'], fuzzy_change['change'])
-
 # controller
 fuzzy_controller = fuzz.ControlSystem([rule1, rule2, rule3])
 
 fuzzy_sim = fuzz.ControlSystemSimulation(fuzzy_controller)
 
-fuzzy_sim.input['error'] = 0.7
+fuzzy_sim.input['error'] = 0.01
 fuzzy_sim.compute()
 print fuzzy_sim.output
 
